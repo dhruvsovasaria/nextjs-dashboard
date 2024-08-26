9d3568e5d51ba60f4f6277dcf6bf24ffe853970c
Dhruv Sovasaria <dhruvsovasaria34@gmail.com>
2024-08-26 12:26:06 +0530

    refactor

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,21 @@
+// import "@/app/ui/global.css";
+// import { inter } from "@/app/ui/fonts";
+
+// export default function RootLayout({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   return (
+//     <html lang="en">
+//       {/* <body className={`${inter.className}`}>{children}</body> */}
+//       <body className={`${inter.className} antialiased`}>{children}</body>
+//     </html>
+//   );
+// }
+import "@/app/ui/global.css";
+import { inter } from "@/app/ui/fonts";
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +23,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
